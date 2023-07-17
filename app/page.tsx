9335933aebb064e261512c35222a0a5ce06e15a8
hidoominik <dominik.choma.41@gmail.com>
2023-07-17 19:18:11 +0200

    adds headlessui, search for cars bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
 import { Hero } from '@/components'
+import CustomFilter from '@/components/CustomFilter'
+import SearchBar from '@/components/SearchBar'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className='py-4 sm:px-16 px-6' id="discover">
+         <div className='home-text-container'>
+          <h1 className='text-4xl font-extrabold'>Car catalogue</h1>
+          <p>Explore different types of cars</p> 
+      </div>
+      <div className='home-filters'>
+          <SearchBar />
+          <div className="home-filters-container">
+              <CustomFilter title="fuel"/>
+              <CustomFilter title="year"/>
+          </div>
+      </div>
+      </div>
+     
     </main>
   )
 }
---
+++ components/SearchBar.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import SearchManufacturer from "./SearchManufacturer";
+import { useState } from "react";
+
+const handleSearch = () => {}
+
+const SearchBar = () => {
+    const [manufacturer, setManufacturer] = useState('');
+  return (
+    <form className='searchbar' onSubmit={handleSearch}>
+        <div className="searchbar-item">
+            <SearchManufacturer 
+                    manufacturer={manufacturer}
+                    setManufacturer = {setManufacturer}
+            />
+        </div>
+       
+
+    </form>
+  )
+}
+
+export default SearchBar
